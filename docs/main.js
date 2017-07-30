e727ca12fe5fc28abd8f7ac585d18b546d293b5a
Anthony <postanthony3000@gmail.com>
2017-07-30 10:34:37 -0500

    added a couple demos changed some css for better site compatibility

--- docs/main.js
+++ docs/main.js
@@ -1,14 +1,15 @@
 window.onload = function() {
 	let cal1 = document.getElementById('cal1')
-	let out = document.getElementById('output')
-	let handle = document.getElementById('show_cal')
-	Cal({element: cal1, handle: handle, on_select: function(dates) {
-		out.value = dates.map((date) => date.format('YYYY-MM-DD')).join(', ')
+	let out1 = document.getElementById('output1')
+	Cal({element: cal1, handle: out1, on_select: function(dates) {
+		out1.value = dates.map((date) => date.format('YYYY-MM-DD')).join(', ')
 	}})
+
 	let cal2 = document.getElementById('cal2')
 	let out2 = document.getElementById('output2')
-	let handle2 = document.getElementById('show_cal2')
-	Cal({element: cal2, handle: handle2, on_select: function(dates) {
+	Cal({element: cal2, handle: out2, on_select: function(dates) {
 		out2.value = dates.map((date) => date.format('YYYY-MM-DD')).join(', ')
 	}, number_of_months: 2})
+
+
 }
